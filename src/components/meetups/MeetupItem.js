0413f7c922277bfdd7758d16a9e4a52dfa9b1bad
woodenbridgewu <woodenbridgewu@gmail.com>
2023-07-08 17:40:12 +0800

    優化刪除流程，使得Delete按下後，被刪除的Item不會留在畫面上

--- src/components/meetups/MeetupItem.js
+++ src/components/meetups/MeetupItem.js
@@ -1,17 +1,25 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import classes from "./MeetupItem.module.css";
 import Card from "../UI/Card";
 import FavoritesContext from "../../store/favorites-context";
 
 function MeetupItem(props) {
   const favoritesCtx = useContext(FavoritesContext);
+  const [isDeleted, setIsDeleted] = useState(false); // 添加状态isDeleted，默认为false
 
   const itemIsFavorite = favoritesCtx.itemIsFavorite(props.id);
+
   function deleteMeetup() {
     fetch(
       `https://react-review-521ee-default-rtdb.firebaseio.com/meetups/${props.id}.json`,
       { method: "DELETE" }
-    );
+    )
+      .then(() => {
+        setIsDeleted(true); // 在删除成功后设置isDeleted为true
+      })
+      .catch((error) => {
+        console.error("Error deleting meetup:", error);
+      });
   }
 
   function toggleFavoriteStatusHandler() {
@@ -26,6 +34,10 @@ function MeetupItem(props) {
         address: props.address,
       });
     }
+  }
+
+  if (isDeleted) {
+    return null; // 如果isDeleted为true，直接返回null，不渲染该项
   }
 
   return (
